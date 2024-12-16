20776d8c2e27575ab878798394805d2732b9d331
Ivan Shumkov <ivan@shumkov.ru>
2024-12-16 18:23:44 +0700

    feat: add single node quorum type

--- json/src/lib.rs
+++ json/src/lib.rs
@@ -2469,6 +2469,7 @@ pub enum QuorumType {
     LlmqDevnetDip0024 = 105,
     LlmqTestPlatform = 106,
     LlmqDevnetPlatform = 107,
+    LlmqSingleNode = 111,
     UNKNOWN = 0,
 }
 
@@ -2489,6 +2490,7 @@ impl From<u32> for QuorumType {
             105 => QuorumType::LlmqDevnetDip0024,
             106 => QuorumType::LlmqTestPlatform,
             107 => QuorumType::LlmqDevnetPlatform,
+            111 => QuorumType::LlmqSingleNode,
             _ => QuorumType::UNKNOWN,
         }
     }
@@ -2512,6 +2514,7 @@ impl Display for QuorumType {
             QuorumType::UNKNOWN => "unknown",
             QuorumType::LlmqTestPlatform => "llmq_test_platform",
             QuorumType::LlmqDevnetPlatform => "llmq_devnet_platform",
+            QuorumType::LlmqSingleNode => "llmq_1_100",
         };
         write!(f, "{}", value)
     }
@@ -2534,6 +2537,7 @@ impl From<&str> for QuorumType {
             "llmq_devnet_dip0024" => QuorumType::LlmqDevnetDip0024,
             "llmq_test_platform" => QuorumType::LlmqTestPlatform,
             "llmq_devnet_platform" => QuorumType::LlmqDevnetPlatform,
+            "llmq_1_100" => QuorumType::LlmqSingleNode,
             _ => QuorumType::UNKNOWN,
         }
     }
